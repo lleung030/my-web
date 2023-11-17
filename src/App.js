8834ed484198a01ab14b0b3b1cbbb516bdd6c024
lleung030 <lucasleung2593@gmail.com>
2023-11-17 15:01:38 -0800

    basic nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import TicTacToe from './components/game/TicTacToe';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 
 function App() {
   return (
-    <TicTacToe />
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <TicTacToe />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/about" className={styles.navLink}>
+            About
+          </Link>
+        </li>
+        {/* Add more navigation items as needed */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
