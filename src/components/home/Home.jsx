70016fd03068c4363ca26cf0589f6c1e7d5a2b0c
lleung030 <lucasleung2593@gmail.com>
2023-11-17 15:54:04 -0800

    homepage img

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,20 @@
-// import styles from "./page.module.css";
-import Me from "../../image/ME.png";
+import React from 'react';
+import Me from '../../image/ME.png';
+import styles from './page.module.css'; // Import your stylesheet
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="">
-        <h1 className="">
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>
           Software Engineer
         </h1>
-        <p className="">
+        <p className={styles.description}>
           A passion for learning, developing, and for God.
         </p>
       </div>
-      <div className="">
-        <image src={Me} alt="" className="" />
+      <div className={styles.imageContainer}>
+        <img src={Me} alt="" className={styles.centeredImage} />
       </div>
     </div>
   );
